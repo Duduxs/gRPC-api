ab573a99dc55477ca2af6a576c7f7a8394b4bb3b
Eduardo José <duduxss3@gmail.com>
2022-09-27 08:19:36 -0300

    main 	:rocket: First cool commit

---
+++ src/main/java/com/edudev/grpcspringapi/GrpcSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.edudev.grpcspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edudev/grpcspringapi/GrpcSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edudev.grpcspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
